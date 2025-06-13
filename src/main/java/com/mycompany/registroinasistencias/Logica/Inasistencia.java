8f9baf26422bbdc6c0de7a3f57425cef0e771de3
Massimiliano <massiperpach@gmail.com>
2025-06-13 18:08:33 -0300

    Creacion de varias clases

---
+++ src/main/java/com/mycompany/registroinasistencias/Logica/Inasistencia.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.registroinasistencias.Logica;
+
+/**
+ *
+ * @author massi
+ */
+public class Inasistencia {
+    
+}
